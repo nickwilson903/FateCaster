2cd6280aac70302ede394e9a213f090b8f25eaf6
nickwilson903 <86578456+nickwilson903@users.noreply.github.com>
2021-12-28 18:25:12 -0500

    Split a ton of code off from the main method, started removing replit specific code.

---
+++ src/bully.py
@@ -0,0 +1,80 @@
+import message_utils
+import user_checker
+import traceback
+import messageparser
+
+no_no_file = open("../resources/no_no_words.txt", "r")
+content = no_no_file.read()
+no_no_list = content.split(",")
+
+# Checks a given message to see if a user on the bully list said a no-no word.
+def checkForNoNo(message):
+    for entry in user_checker.fetchList():
+        if str(message.author.id) in str(entry):
+            for no_no in no_no_list:
+                if no_no in str(message.content).lower():
+                    response = ("<@" + str(message.author.id) + ">" + " shut the fuck up.")
+                    return response
+                else:
+                    return None
+        else:
+            return None
+
+# Adds someone to the bully list based on an @ extracted from the message
+def addBully(client, message):
+      if "daddies" in [role.name.lower() for role in message.author.roles]:
+        if (len(message.mentions) > 0):
+          victim=message.mentions[0].id
+          print("Adding someone to the bully list.")
+          try:
+            print("Extracting name from message" + str(victim))
+            #victim = messageparser.extractBullyName(message.content)
+            member = await client.fetch_user(victim)
+            victim_name = member.name
+            user_checker.addUserToList(victim)
+            response = message_utils.format_response("Added " + victim_name + " to bully list.")
+            return response
+          except:
+            traceback.print_exc()
+            response = message_utils.format_response("Invalid syntax, please type !bully add @user")
+            return response
+        else:
+          response = message_utils.format_response("No users specified.")
+          return response
+
+# Fetches the list of people to bully
+def getBullyList(client):
+    print("Returning list of victims.")
+    roster = []
+    roster_of_ids = user_checker.fetchList()
+    for entry in roster_of_ids:
+        member = await client.fetch_user(int(entry))
+        name = member.name
+        roster.append(name)
+    print(roster)
+    formatted_message = "List of victims: "
+    for entry in roster:
+        print(entry)
+        formatted_message += ("\n" + str(roster.index(entry)) + ": " + entry)
+    response = message_utils.format_response(formatted_message)
+    return response
+
+# Removes someone from the bully list based on an index value retrieved from the message.
+def removeBully(message):
+    if "daddies" in [role.name.lower() for role in message.author.roles]:
+        print("Removing victim from list.")
+        try:
+            print("Extracting index from message")
+            victim = messageparser.extractBullyName(message.content)
+            user_checker.removeUserFromList(victim)
+            response = message_utils.format_response("Removed entry: " + victim)
+            return response
+        except:
+            traceback.print_exc()
+            response = message_utils.format_response(
+                "Invalid removal request. Type !bully list to get a list of users to keep track of, then !bully remove NUMBER to remove an entry from the list.")
+            return response
+    else:
+        response = message_utils.format_response("No, cry about it.")
+        return response
+
--- src/roller.py
+++ src/roller.py
@@ -1,4 +1,7 @@
 import dice
+import messageparser
+import message_utils
+import traceback
 
 #TODO: Add additional attributes for fudgeModifier and fudgeCount
 
@@ -22,6 +25,23 @@ class Roller:
     @getModifier.setter
     def seModifier(self, mod):
         self.modifier = mod
+
+    def identifyRoll(self, message):
+        try:
+            roll_elements = messageparser.getRollContent(message.content)
+            self.command = roll_elements[0]
+            self.modifier = roll_elements[1]
+            result = self.rollTheFucker()
+            if "-" not in roll_elements[1]:
+                roll_elements[1] = ("+ " + str(roll_elements[1]))
+            response = message_utils.format_response(
+                roll_elements[2] + "\n" + roll_elements[0] + " " + roll_elements[1] + " = " + str(result))
+            return response
+
+        except:
+            traceback.print_exc()
+            response = message_utils.format_response("Invalid roll syntax, please type `!r XdY` with an optional `+` or `-` `Z` numerical modifier.")
+            return response
 
     def rollTheFucker(self):
       #takes in a list of dice elements (a command and a modifier), rolls them, and 
